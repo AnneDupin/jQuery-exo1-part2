2fbf4834fb3e45b485aee67c0db7757c64ead4bf
Anne <anne.dupin91@gmail.com>
2021-04-28 15:12:28 +0200

    First

---
+++ script.js
@@ -0,0 +1,23 @@
+// $(document).on('click', '#clique-moi', function(){
+// alert("HAHAHAH butt{on is cliked");
+// });
+
+// ou 
+
+// $(document).ready(function(){
+//     $("#clique-moi").click(function(){
+//         alert("Hahaha button is clicked");
+//     });
+// });
+
+//ou encore
+
+// $('#clique-moi').click(function(event){
+// alert("HAHAHA button is clicked")
+// })
+
+// last but not least
+
+$("#clique-moi").click(function(){
+alert("Button is clicked too")
+});
